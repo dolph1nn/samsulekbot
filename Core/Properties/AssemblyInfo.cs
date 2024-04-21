fc8981e052ed70f23ea68bee1f825d66529eddcd
Dolph1n <daniel@lunarcolony.dev>
2024-04-20 19:24:39 -0700

    [BUILD] okay so i HAVE to use packages.config

--- Core/Properties/AssemblyInfo.cs
+++ Core/Properties/AssemblyInfo.cs
@@ -1,32 +1,16 @@
-﻿/* This file is part of SamSulekBot.
- *
- * SamSulekBot is free software: you can redistribute it and/or modify it
- * under the terms of the GNU General Public License as published by the
- * Free Software Foundation, either version 3 of the License, or (at your
- * option) any later version.
- *
- * SamSulekBot is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or 
- * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
- * more details.
- *
- * You should have received a copy of the GNU General Public License along
- * with SamSulekBot. If not, see <https://www.gnu.org/licenses/>.
- */
-
-using System.Reflection;
+﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("SamSulekBot Core")]
-[assembly: AssemblyDescription("Core Library for SamSulekBot")]
+[assembly: AssemblyTitle("Core")]
+[assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("SamSulekBot")]
-[assembly: AssemblyCopyright("Copyright © 2024 dolphin")]
+[assembly: AssemblyProduct("Core")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -36,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("c8461cef-c28b-4887-93ee-3fd050ee8842")]
+[assembly: Guid("cd19eb35-4385-419a-92cc-6f7ed3cf8e76")]
 
 // Version information for an assembly consists of the following four values:
 //
