056bd5ad6f53869ed236cba8efc189cbd323af37
Dolph1n <daniel@lunarcolony.dev>
2024-04-21 11:17:37 -0700

    add license to SSBService.cs

--- SSBService/SSBService.cs
+++ SSBService/SSBService.cs
@@ -1,4 +1,20 @@
-﻿#pragma warning disable CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
+﻿/* This file is part of SamSulekBot.
+ *
+ * SamSulekBot is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version.
+ *
+ * SamSulekBot is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or 
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with SamSulekBot. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#pragma warning disable CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
 using System.ServiceProcess;
 
 namespace SSB.Service
